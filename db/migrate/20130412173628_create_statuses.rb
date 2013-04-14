4c9c88bce6be0afb20c64cadab75455d869dc50b
mikehan <keke231@qq.com>
2013-04-14 19:04:26 +0800

    Customizing Forms

--- db/migrate/20130412173628_create_statuses.rb
+++ db/migrate/20130412173628_create_statuses.rb
@@ -1,7 +1,7 @@
 class CreateStatuses < ActiveRecord::Migration
   def change
     create_table :statuses do |t|
-      t.string :name
+      t.string :user_id
       t.text :content
 
       t.timestamps
